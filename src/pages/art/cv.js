799aef35fe065ff15ebbb119a3d713d259466ee2
Patrick Kalita <patrick.kalita@gmail.com>
2021-11-07 19:44:10 -0800

    CV page formatting

--- src/pages/art/cv.js
+++ src/pages/art/cv.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { useEffect, useState } from 'react'
 import ArtPage from '../../components/art/ArtPage'
 
 const Heading = props => (
@@ -9,6 +8,7 @@ const Heading = props => (
         sx={{
             fontSize: 2,
             fontWeight: 'bold',
+            marginBottom: 2,
         }}
     />
 )
@@ -19,15 +19,28 @@ const Item = ({year, title, location, ...rest}) => (
     </div>
 )
 
+const Section = props => (
+    <section 
+        {...props} 
+        sx={{
+            marginBottom: 4
+        }} 
+    />
+)
+
 const ArtCV = () => {
     return (
         <ArtPage>
-            <Heading>Group Exhibitions</Heading>
-            <Item year={2020} title='Spring Exhibition' location='Emerald Art Center, Springfield OR' />
-            <Item year={2020} title='Zone 4 All' location='New Zone Gallery, Eugene OR' />
-            <Heading>Education</Heading>
-            <Item year={2008} title='MS Electrical Engineering' location='Purdue University, West Lafayette IN' />
-            <Item year={2020} title='BS Electrical Engineering' location='Rose-Hulman Institute of Technology, Terre Haute IN' />
+            <Section>
+                <Heading>Group Exhibitions</Heading>
+                <Item year={2020} title='Spring Exhibition' location='Emerald Art Center, Springfield OR' />
+                <Item year={2020} title='Zone 4 All' location='New Zone Gallery, Eugene OR' />
+            </Section>
+            <Section>
+                <Heading>Education</Heading>
+                <Item year={2008} title='MS Electrical Engineering' location='Purdue University, West Lafayette IN' />
+                <Item year={2006} title='BS Electrical Engineering' location='Rose-Hulman Institute of Technology, Terre Haute IN' />
+            </Section>
         </ArtPage>
     )
 }
